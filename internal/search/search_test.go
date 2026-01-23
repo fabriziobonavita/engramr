4a06a93a13aecd9b31a1bbb04f4dd756ec7e9b07
Fabrizio Bonavita <fabrizioschmidt@gmail.com>
2026-01-23 13:27:02 +0100

    -minor cleanup

--- internal/search/search_test.go
+++
@@ -1,4 +0,0 @@
-package search
-
-// search_test.go is intentionally minimal - most tests are in integration tests.
-// This file can be used for search-specific tests (Query, Snippet, etc.) in the future.
